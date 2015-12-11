5db6ea9d79d0ca732ba1937df05ab05db35e3307
Ohohcakester <ohohgit@gmail.com>
2015-12-11 15:59:54 +0800

    Maze Manager and Grid Graph

---
+++ Assets/Gameplay/Scripts/GridGraph.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Net.Configuration;
+
+public class GridGraph
+{
+    private bool[,] tileGrid;
+    private bool[,] pfGrid;
+    public int scale { get; private set; }
+    public int sizeX { get; private set; }
+    public int sizeY { get; private set; }
+    public int pfSizeX { get; private set; }
+    public int pfSizeY { get; private set; }
+    public float realMinX;
+    public float realMinY;
+    public float width;
+    public float height;
+
+    public void Initialise(bool[,] tiles, float realMinX, float realMinY, float width, float height)
+    {
+        this.realMinX = realMinX;
+        this.realMinY = realMinY;
+        this.width = width;
+        this.height = height;
+        this.sizeX = tiles.GetLength(0);
+        this.sizeY = tiles.GetLength(1);
+        this.pfSizeX = sizeX * scale;
+        this.pfSizeY = sizeY * scale;
+        tileGrid = tiles;
+        pfGrid = new bool[pfSizeX,pfSizeY];
+        ConfigurePfGrid();
+    }
+
+    private void ConfigurePfGrid()
+    {
+        for (int y = 0; y < pfSizeY; ++y)
+        {
+            for (int x = 0; x < pfSizeX; ++x)
+            {
+                pfGrid[x, y] = checkIsBlockedPfTile(x, y) ||
+                               checkIsBlockedPfTile(x - 1, y) ||
+                               checkIsBlockedPfTile(x, y - 1) ||
+                               checkIsBlockedPfTile(x + 1, y) ||
+                               checkIsBlockedPfTile(x, y + 1);
+            }
+        }
+    }
+
+    private bool checkIsBlockedPfTile(int pfX, int pfY)
+    {
+        if (pfX < 0) return true;
+        if (pfY < 0) return true;
+        if (pfX >= pfSizeX) return true;
+        if (pfY >= pfSizeY) return true;
+        return tileGrid[pfX/scale, pfY/scale];
+    }
+
+    private bool IsBlockedPfTile(int pfx, int pfy)
+    {
+        if (pfx < 0) return true;
+        if (pfy < 0) return true;
+        if (pfx >= pfSizeX) return true;
+        if (pfy >= pfSizeY) return true;
+        return pfGrid[pfx, pfy];
+    }
+
+    private bool IsBlockedCoordinate(int pfx, int pfy)
+    {
+        return IsBlockedPfTile(pfx, pfy) &&
+               IsBlockedPfTile(pfx-1, pfy) &&
+               IsBlockedPfTile(pfx, pfy-1) &&
+               IsBlockedPfTile(pfx-1, pfy-1);
+    }
+
+    /// <summary>
+    /// Input: Unity Coordinates. Output: Pf Grid Coordinates.
+    /// </summary>
+    private void ToPfGridCoordinates(float x, float y, out int gx, out int gy)
+    {
+        gx = 0;
+        gy = 0;
+    }
+
+    /// <summary>
+    /// Input: Pf Grid Coordinates. Output: Unity Coordinates.
+    /// </summary>
+    private void ToRealCoordinates(int gx, int gy, out float x, out float y)
+    {
+        x = width * gx / pfSizeX;
+        y = height * gy / pfSizeY;
+    }
+
+    public Vector3 GetMoveDirection(float currX, float currY, float targetX, float targetY)
+    {
+        return Vector3.zero;
+    }
+}
---
+++ Assets/Gameplay/Scripts/MazeManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MazeManager : MonoBehaviour
+{
+    [SerializeField]
+    private float minX;
+    [SerializeField]
+    private float minY;
+    [SerializeField]
+    private float tileWidth;
+    [SerializeField]
+    private float tileHeight;
+
+    private float width;
+    private float height;
+
+    private GridGraph gridGraph;
+    private bool[,] platforms;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void InitialiseGridGraph(String text)
+    {
+        var lines = text.Split('\n');
+        int nCols = lines[0].Split(' ').Length;
+        int nRows = lines.Length;
+        var isBlocked = new bool[nCols,nRows];
+        platforms = new bool[nCols,nRows];
+
+        for (int y = 0; y < nRows; ++y)
+        {
+            var chars = lines[y].Split(' ');
+            for (int x = 0; x < nCols; ++x)
+            {
+                switch (chars[x].ToLower())
+                {
+                    case "x":
+                        isBlocked[x, y] = false;
+                        platforms[x, y] = false;
+                        break;
+                    case "l":
+                        isBlocked[x, y] = true;
+                        platforms[x, y] = false;
+                        break;
+                    case "t":
+                        isBlocked[x, y] = true;
+                        platforms[x, y] = false;
+                        break;
+                    case "r":
+                        isBlocked[x, y] = true;
+                        platforms[x, y] = false;
+                        break;
+                    case "b":
+                        isBlocked[x, y] = true;
+                        platforms[x, y] = false;
+                        break;
+                }
+            }
+        }
+
+        width = tileWidth*nCols;
+        height = tileHeight*nRows;
+
+        gridGraph = new GridGraph();
+        gridGraph.Initialise(isBlocked, minX, minY, width, height);
+    }
+}
