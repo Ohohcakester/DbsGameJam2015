dca92516c042ea2c3de5573b4d580437f79ee320
Ohohcakester <ohohgit@gmail.com>
2015-12-11 13:38:25 +0800

    Player

---
+++ Assets/Gameplay/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private const float WalkSpeed = 5;
+    private const float JumpSpeed = 8;
+    private Rigidbody2D rigidbody2D;
+    private PlayerGroundCollider groundCollider;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Initialise();
+	}
+
+    private void Initialise()
+    {
+        if (rigidbody2D != null) return;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        groundCollider = transform.FindChild("GroundCollider").GetComponent<PlayerGroundCollider>();
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+	    float vx = 0;
+
+	    if (Input.GetKey(KeyCode.LeftArrow))
+	    {
+	        vx -= WalkSpeed;
+	    }
+        if (Input.GetKey(KeyCode.RightArrow))
+	    {
+	        vx += WalkSpeed;
+	    }
+        if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        if (IsOnPlatform())
+	        {
+	            Jump();
+	        }
+	    }
+
+	    rigidbody2D.velocity = OhVec.SetX(rigidbody2D.velocity, vx);
+	}
+
+    private bool IsOnPlatform()
+    {
+        return groundCollider.OnGround;
+    }
+
+    private void Jump()
+    {
+        rigidbody2D.velocity = OhVec.SetY(rigidbody2D.velocity, JumpSpeed);
+    }
+}
---
+++ Assets/Gameplay/Scripts/PlayerGroundCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGroundCollider : MonoBehaviour
+{
+    [SerializeField]
+    private int PlatformLayer;
+    public bool OnGround { get; private  set; }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer != PlatformLayer) return;
+        OnGround = true;
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer != PlatformLayer) return;
+        OnGround = false;
+    }
+}
