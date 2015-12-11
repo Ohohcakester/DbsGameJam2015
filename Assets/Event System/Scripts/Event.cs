16420519c907e7f89a1f1c767128ff2e7c0b192e
Dartteon <a0122545@u.nus.edu>
2015-12-11 13:05:15 +0800

    Started on Event system
    Changelog:
    1) Event class added
    2) EventRepresenter object added
    //EventRepresenter is a GameObject representation that will be displayed as the UI

---
+++ Assets/Event System/Scripts/Event.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Event{
+	public string eventName;
+	public string eventDescription;
+
+	//Constructor
+	public Event (string name){
+		eventName = name;
+		if (name == "Test") {
+			eventDescription = "Test Display Text";
+		}
+	}
+
+	public void configureUIEventObject(GameObject eventObj){
+		eventObj.transform.Find ("TextCanvas").GetComponentInChildren<Text> ().text = eventDescription;
+	}
+
+
+}
