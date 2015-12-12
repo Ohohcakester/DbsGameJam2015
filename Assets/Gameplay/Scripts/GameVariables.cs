d299336823f26238f1a91f3bbaf0fc2e7b3785d0
Ohohcakester <ohohgit@gmail.com>
2015-12-12 14:23:24 +0800

    Update SequenceManager

--- Assets/Gameplay/Scripts/GameVariables.cs
+++ Assets/Gameplay/Scripts/GameVariables.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 
 public class GameVariables
 {
-    public float enemyAggressiveness = 0.5f;
     public float baseDetectionRange = 10;
     public float baseDetectionDistance = 10;
     public int nEnemies;
     public int nCrabs;
-    public float multiplier;
-    public float undercurrent;
+    public float multiplier = 1f;
+    public float undercurrent = 0f;
     public float visionRadius;
-    public float respawnRate;
+    public float itemRespawnRate;
 
+    public float turnSpeed = 7.7f;
+    public float moveSpeed = 6f;
+    public float baseAggressiveness = 0.5f;
+    public float decisionTime = 0f;
+    public float decisionTimeExtra = 0.5f;
+
+    public void ResetGameBuffs()
+    {
+
+    }
+
+    public void ApplyDifficultyChange(float timePassed)
+    {
+
+    }
 }
--- Assets/Gameplay/Scripts/SequenceManager.cs
+++ Assets/Gameplay/Scripts/SequenceManager.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Event = Orb.Event;
 
 class SequenceManager
 {
-    private EventStatus eventStatus;
     private GameController gameController;
     public GameVariables gameVariables { get; private set; }
 
@@ -12,19 +12,76 @@ class SequenceManager
     private float nextEventHappenTime;
     private float luck;
 
+    private float previousFrameTime;
+
+    private List<ActiveEvent> activeEvents;
+
     public SequenceManager()
     {
-        eventStatus = new EventStatus();
         gameController = Camera.main.GetComponent<GameController>();
         gameVariables = new GameVariables();
     }
 
     public void Update()
     {
-        /*if (Time.time > nextEventHappenTime)
+        float dTime = Time.time - previousFrameTime;
+        previousFrameTime = Time.time;
+        RunEvents();
+
+        if (HasMaxEvents())
         {
-            Event ev;
-        }*/
+            nextEventHappenTime += dTime;
+        }
+        else
+        {
+            if (Time.time > nextEventHappenTime)
+            {
+                Event ev = null; // pop from queue
+                if (WillEventHappenRandom(ev.getEventType()))
+                {
+                    StartEvent(ev);
+                    AdjustLuck(ev.getEventType());
+                }
+                else
+                {
+                    // Don't Start Event
+                }
+
+            }
+        }
+    }
+
+    private bool HasMaxEvents()
+    {
+        return activeEvents.Count >= 3;
+    }
+
+    private void RunEvents()
+    {
+        for (int i = 0; i < activeEvents.Count; ++i)
+        {
+            var activeEvent = activeEvents[i];
+            if (Time.time > activeEvent.expireTime)
+            {
+                activeEvent.Expire();
+            }
+        }
+    }
+
+    private void StartEvent(Event ev)
+    {
+        ActiveEvent activeEvent = new ActiveEvent(ev, EventDuration(ev));
+        activeEvents.Add(activeEvent);
+    }
+
+    private float EventDuration(Event ev)
+    {
+        return 10f;
+    }
+
+    private void ClearInactiveEvents()
+    {
+        activeEvents.RemoveAll(ev => ev.isExpired);
     }
 
     /// <summary>
@@ -73,26 +130,44 @@ class SequenceManager
         luck += 0;
     }
 
-    private void ResetGameBuffs()
-    {
-        
-    }
-
     private void ApplyBuffs()
     {
-        ResetGameBuffs();
-        foreach (var buff in eventStatus.events)
+        gameVariables.ResetGameBuffs();
+        foreach (var activeEvent in activeEvents)
         {
-            if (eventStatus.status[buff])
-            {
-                Apply(buff);
-            }
+            ApplyEffect(activeEvent.ev.getEventType());
         }
+        gameVariables.ApplyDifficultyChange(Time.time - startTime);
     }
 
-    private void Apply(OrbEventEnumerator.Event buff)
+    private void ApplyEffect(OrbEventEnumerator.Event buff)
     {
-        
+
     }
 
+
+    private void StartEffect(OrbEventEnumerator.Event buff)
+    {
+
+    }
+
+}
+
+internal class ActiveEvent
+{
+    public Event ev { get; private set; }
+    public float expireTime { get; private set; }
+    public bool isExpired { get; private set; }
+
+    public ActiveEvent(Event ev, float duration)
+    {
+        this.ev = ev;
+        this.expireTime = Time.time + duration;
+        isExpired = false;
+    }
+
+    public void Expire()
+    {
+        isExpired = true;
+    }
 }
