565cfeea49e4bf0565f0bb5940e018df862c56e2
Ohohcakester <ohohgit@gmail.com>
2015-12-12 22:22:10 +0800

    Actual event strings

--- Assets/Event System/Scripts/Event.cs
+++ Assets/Event System/Scripts/Event.cs
@@ -51,33 +51,33 @@ namespace Orb {
 
 	    public static string getEventDescription(OrbEventEnumerator.Event ev)
 	    {
-	        return ev.ToString();
+	        //return ev.ToString();
 
-	        /*switch (ev)
+	        switch (ev)
 	        {
+                case OrbEventEnumerator.Event.BonusStarlight: return "Treasure!";
+                case OrbEventEnumerator.Event.Multiplier2: return "2x Orb\nMultiplier";
+                case OrbEventEnumerator.Event.Multiplier3: return "3x Orb\nMultiplier";
+                case OrbEventEnumerator.Event.LessJellyfish: return "Less\nJellyfish";
+                case OrbEventEnumerator.Event.Multiplier5: return "5x Orb\nMultiplier";
+                case OrbEventEnumerator.Event.EnemiesRun: return "Terrified\nJellyfish";
+                case OrbEventEnumerator.Event.LessCrabs: return "Less\nCrabs";
+                case OrbEventEnumerator.Event.MoreStarlight: return "Abundant\nStarlight";
 
-                case OrbEventEnumerator.Event.BonusStarlight: return 2.9;
-                case OrbEventEnumerator.Event.Multiplier2: return 2;
-                case OrbEventEnumerator.Event.Multiplier3: return 3;
-                case OrbEventEnumerator.Event.LessJellyfish: return 3.7;
-                case OrbEventEnumerator.Event.Multiplier5: return 5;
-                case OrbEventEnumerator.Event.EnemiesRun: return 7.9;
-                case OrbEventEnumerator.Event.LessCrabs: return 2.1;
-                case OrbEventEnumerator.Event.MoreStarlight: return 2.4;
-
-                case OrbEventEnumerator.Event.SquidInk: return -1.7;
-                case OrbEventEnumerator.Event.FastJellyfish: return -4.6;
-                case OrbEventEnumerator.Event.MoreJellyfish: return -5.3;
-                case OrbEventEnumerator.Event.FastCrabs: return -1.1;
-                case OrbEventEnumerator.Event.MoreCrabs: return -2.1;
-                case OrbEventEnumerator.Event.UndercurrentLeft: return -1.4;
-                case OrbEventEnumerator.Event.UndercurrentRight: return -1.4;
-                case OrbEventEnumerator.Event.AggressiveJellyfish: return -5.1;
-                case OrbEventEnumerator.Event.Multiplier0_2: return -5;
-                case OrbEventEnumerator.Event.Multiplier0_5: return -3;
-                case OrbEventEnumerator.Event.LessStarlight: return -1.9;
-                case OrbEventEnumerator.Event.Multiplier0_8: return -2;
-	        }*/
+                case OrbEventEnumerator.Event.SquidInk: return "Squid\nSplatter";
+                case OrbEventEnumerator.Event.FastJellyfish: return "Speedy\nJellyfish";
+                case OrbEventEnumerator.Event.MoreJellyfish: return "Jellyfish\nSeason";
+                case OrbEventEnumerator.Event.FastCrabs: return "Speedy\nCrabs";
+                case OrbEventEnumerator.Event.MoreCrabs: return "Crab\nSeason";
+                case OrbEventEnumerator.Event.UndercurrentLeft: return "Fast\nCurrent";
+                case OrbEventEnumerator.Event.UndercurrentRight: return "Fast\nCurrent";
+                case OrbEventEnumerator.Event.AggressiveJellyfish: return "They can\nsee you";
+                case OrbEventEnumerator.Event.Multiplier0_2: return "0.2x Orb\nMultiplier";
+                case OrbEventEnumerator.Event.Multiplier0_5: return "0.5x Orb\nMultiplier";
+                case OrbEventEnumerator.Event.LessStarlight: return "Starlight\nScarcity";
+                case OrbEventEnumerator.Event.Multiplier0_8: return "0.8x Orb\nMultiplier";
+	        }
+	        return ev.ToString();
 	    }
 
     }
