ba2931d0b5d70bc66b4dad08daa40b95f0935b24
Dartteon <a0122545@u.nus.edu>
2015-12-12 12:20:10 +0800

    Added in active events portion for EventScreen

--- Assets/Event System/Scripts/EventRepresenter.cs
+++ Assets/Event System/Scripts/EventRepresenter.cs
@@ -4,23 +4,35 @@ using System.Collections;
 public class EventRepresenter : MonoBehaviour {
 	private Vector2 targetPos;
 	private float speed = 0.1f;
-	private OrbEventEnumerator.Event ev;
+	private Event ev;
+	private bool isSelfDestructing = false;
 
 	// Update is called once per frame
 	void Update () {
 		Vector2 currPos = this.transform.localPosition;
 		this.transform.localPosition = Vector2.Lerp (currPos, targetPos, speed);
+
+		if (isSelfDestructing) {
+			if ((currPos - targetPos).magnitude <= 0.3f) {
+				Destroy (this.gameObject);
+			}
+		}
 	}
 
 	public void setPos(Vector2 pos){
 		targetPos = pos;
 	}
 
-	public void includeEvent(OrbEventEnumerator.Event _event){
+	public void includeEvent(Event _event){
 		ev = _event;
 	}
 
-	public OrbEventEnumerator.Event getEvent(){
+	public void moveTowardsThenDestroySelf(Vector2 pos){
+		isSelfDestructing = true;
+		targetPos = pos;
+	}
+
+	public Event getEvent(){
 		return ev;
 	}
 }
--- Assets/Gameplay/Scripts/CameraFollow.cs
+++ Assets/Gameplay/Scripts/CameraFollow.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 	private GameObject player;
 	private Vector2 targetPos;
+	private Vector2 offset = new Vector2 (+.9f, 0);
 	public float panSpeed = 0.1f;
 
 	public void setPlayer (GameObject plyr){
@@ -16,7 +17,8 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (player != null) {
-			targetPos = player.transform.position;
+			Vector2 playerPos = player.transform.position;
+			targetPos = playerPos + offset;
 			this.transform.position = Vector3.Lerp(this.transform.position, OhVec.toVector3 (targetPos, -10f), panSpeed);
 		}
 	}
