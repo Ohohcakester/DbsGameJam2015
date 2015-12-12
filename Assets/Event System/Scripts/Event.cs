a8c98e8ae2572cfd3815888246afe8cefbe1e1f9
Ohohcakester <ohohgit@gmail.com>
2015-12-12 13:03:56 +0800

    Base Code of SequenceManager

--- Assets/Event System/Scripts/Event.cs
+++ Assets/Event System/Scripts/Event.cs
@@ -2,85 +2,124 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Event{
-	public string eventName;
-	public string eventDescription;
 
-	private OrbEventEnumerator.Event eventType;
+namespace Orb
+{
+    public class Event
+    {
+        public string eventName;
+        public string eventDescription;
 
-	//Constructor
-	public Event (OrbEventEnumerator.Event ev) {
-		eventType = ev;
-		eventDescription = getEventDescription(eventType);
-		eventName = eventDescription;
-	}
+        private OrbEventEnumerator.Event eventType;
 
-	public Event() {
-		eventType = getRandomEvent ();
+        //Constructor
+        public Event(OrbEventEnumerator.Event ev)
+        {
+            eventType = ev;
+            eventDescription = getEventDescription(eventType);
+            eventName = eventDescription;
+        }
+
+        public Event()
+        {
+            eventType = getRandomEvent();
 //		int randomEventIndex = Random.Range (0, eventNamesArray.Length);
 //		eventName = eventNamesArray [randomEventIndex];
 //		eventDescription = eventNamesArray [randomEventIndex];
 //		Debug.Log(getEventDescription(eventType));
-		eventDescription = getEventDescription(eventType);
-		eventName = eventDescription;
-	}
+            eventDescription = getEventDescription(eventType);
+            eventName = eventDescription;
+        }
 
-	public void configureUIEventObject(GameObject eventObj) {
+        public void configureUIEventObject(GameObject eventObj)
+        {
 //		Debug.Log ("Configuring" + eventDescription);
-		eventObj.transform.Find ("TextCanvas").GetComponentInChildren<Text> ().text = eventDescription;
-	}
+            eventObj.transform.Find("TextCanvas").GetComponentInChildren<Text>().text = eventDescription;
+        }
 
 //	public void 
 
-	public OrbEventEnumerator.Event getEventType() {
-		return this.eventType;
-	}
+        public OrbEventEnumerator.Event getEventType()
+        {
+            return this.eventType;
+        }
 
-	public OrbEventEnumerator.Event getRandomEvent(){
-		int dieRoll = Random.Range (0, 100);
+        public OrbEventEnumerator.Event getRandomEvent()
+        {
+            int dieRoll = Random.Range(0, 100);
 //		Debug.Log (dieRoll);
-		OrbEventEnumerator.Event type;
+            OrbEventEnumerator.Event type;
 
-		if (dieRoll <= 30) {
+            if (dieRoll <= 30)
+            {
 //			Debug.Log ("Mult");
-			type = OrbEventEnumerator.Event.Multiplier;
-		} else if (dieRoll <= 50) {
+                type = OrbEventEnumerator.Event.Multiplier;
+            }
+            else if (dieRoll <= 50)
+            {
 //			Debug.Log ("Meteor");
-			type = OrbEventEnumerator.Event.Meteor;
-		} else if (dieRoll <= 60) {
-			type = OrbEventEnumerator.Event.Crabby;
-		} else if (dieRoll <= 70) {
-			type = OrbEventEnumerator.Event.Jelly;
-		} else if (dieRoll <= 80) {
-			type = OrbEventEnumerator.Event.Tsunami;
-		} else if (dieRoll <= 90) {
-			type = OrbEventEnumerator.Event.Test2;
-		} else {
-			type = OrbEventEnumerator.Event.Test3;
-		}
+                type = OrbEventEnumerator.Event.Meteor;
+            }
+            else if (dieRoll <= 60)
+            {
+                type = OrbEventEnumerator.Event.Crabby;
+            }
+            else if (dieRoll <= 70)
+            {
+                type = OrbEventEnumerator.Event.Jelly;
+            }
+            else if (dieRoll <= 80)
+            {
+                type = OrbEventEnumerator.Event.Tsunami;
+            }
+            else if (dieRoll <= 90)
+            {
+                type = OrbEventEnumerator.Event.Test2;
+            }
+            else
+            {
+                type = OrbEventEnumerator.Event.Test3;
+            }
 //		Debug.Log (dieRoll + " " + type.ToString());
-		return type;
-	}
+            return type;
+        }
 
-	public static string getEventDescription(OrbEventEnumerator.Event ev){
+        public static string getEventDescription(OrbEventEnumerator.Event ev)
+        {
 //		Debug.Log (ev.ToString ());
-		if (ev == OrbEventEnumerator.Event.Multiplier) {
-			return "Multiplier";
-		} else if (ev == OrbEventEnumerator.Event.Meteor) {
-			return "Meteor";
-		} else if (ev == OrbEventEnumerator.Event.Crabby) {
-			return "Crabby";
-		} else if (ev == OrbEventEnumerator.Event.Jelly) {
-			return "Jelly";
-		} else if (ev == OrbEventEnumerator.Event.Tsunami) {
-			return "Tsunami";
-		} else if (ev == OrbEventEnumerator.Event.Test2) {
-			return "Test2";
-		} else if (ev == OrbEventEnumerator.Event.Test3) {
-			return "Test3";
-		} else {
-			return "Unfound";
-		}
-	}
+            if (ev == OrbEventEnumerator.Event.Multiplier)
+            {
+                return "Multiplier";
+            }
+            else if (ev == OrbEventEnumerator.Event.Meteor)
+            {
+                return "Meteor";
+            }
+            else if (ev == OrbEventEnumerator.Event.Crabby)
+            {
+                return "Crabby";
+            }
+            else if (ev == OrbEventEnumerator.Event.Jelly)
+            {
+                return "Jelly";
+            }
+            else if (ev == OrbEventEnumerator.Event.Tsunami)
+            {
+                return "Tsunami";
+            }
+            else if (ev == OrbEventEnumerator.Event.Test2)
+            {
+                return "Test2";
+            }
+            else if (ev == OrbEventEnumerator.Event.Test3)
+            {
+                return "Test3";
+            }
+            else
+            {
+                return "Unfound";
+            }
+        }
 
+    }
 }
--- Assets/Gameplay/Scripts/SequenceManager.cs
+++ Assets/Gameplay/Scripts/SequenceManager.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using Event = Orb.Event;
 
 class SequenceManager
 {
     private EventStatus eventStatus;
     private GameController gameController;
     public GameVariables gameVariables { get; private set; }
+
+    private float startTime;
+    private float nextEventHappenTime;
+    private float luck;
 
     public SequenceManager()
     {
@@ -16,7 +21,56 @@ class SequenceManager
 
     public void Update()
     {
-        
+        /*if (Time.time > nextEventHappenTime)
+        {
+            Event ev;
+        }*/
+    }
+
+    /// <summary>
+    /// -infty: 0
+    /// infty: 1
+    /// 0: 1/2
+    /// </summary>
+    private float Sigmoid(float value)
+    {
+        return 1/(1 + Mathf.Exp(-value));
+    }
+
+    /// <summary>
+    /// -infty: probability 0 of being true.
+    /// infty: probability 1 of being true
+    /// </summary>
+    private bool SigmoidRandom(float value)
+    {
+        return Random.Range(0, 1f) < Sigmoid(value);
+    }
+
+    private bool WillEventHappenRandom(OrbEventEnumerator.Event ev)
+    {
+        return Random.Range(0, 1f) < 0.5f;
+    }
+
+    public void AddNewEvent()
+    {
+        if (SigmoidRandom(luck))
+        {
+            // Negative Things
+            
+        }
+        else
+        {
+            // Positive Things
+
+        }
+    }
+
+    public void AdjustLuck(OrbEventEnumerator.Event ev)
+    {
+        switch (ev)
+        {
+        }
+        luck += 0;
     }
 
     private void ResetGameBuffs()
