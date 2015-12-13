9287f2f5535c471f4d400ee59c8f83c359e0d0fd
Dartteon <a0122545@u.nus.edu>
2015-12-13 15:06:13 +0800

    Implemented working game over screen with buttons to go back to main/restart

---
+++ Assets/Gameplay/Scripts/ButtonLevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLevelLoader : MonoBehaviour {
+	public string levelName;
+
+	void OnMouseDown(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (levelName);
+	//	Application.LoadL
+	}
+}
